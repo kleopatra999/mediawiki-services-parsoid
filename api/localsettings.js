c3ffafb877d165e065a5562d390c91f4319edd58
Inez Korczynski <inez@wikia-inc.com>
2014-05-05 20:11:03 +0000

    Initial commit of default localsettings.js

---
+++ api/localsettings.js
@@ -0,0 +1,11 @@
+/*
+ * This is a sample configuration file.
+ *
+ * Copy this file to localsettings.js and edit that file to fit your needs.
+ *
+ * Also see the file ParserService.js for more information.
+ */
+
+exports.setup = function( parsoidConfig ) {
+	parsoidConfig.useSelser = true;
+};
